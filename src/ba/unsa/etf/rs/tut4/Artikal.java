f4e3f7981cb8af6d5057747913c668aab30e4fa6
Avujcic1 <avujcic1@etf.unsa.ba>
2020-03-25 18:37:50 +0100

    Setters/getters

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rs.tut4;
 
 public class Artikal {
+    private String sifra;
+    private String naziv;
+    private Double cijena;
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setCijena(Double cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public Double getCijena() {
+        return cijena;
+    }
+
+    public void setArtikal(String artikal) {
+        
+    }
+
+
 }
